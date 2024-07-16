2b6ce73dfbbae20842bdcb6233f6c5f4a7a6c6ca
Django Merope <dmerope@gmail.com>
2024-07-16 18:41:12 +0900

    Removed useless OpenAPI stub.

--- src/routes/(api)/api/v1/_schema/+server.ts
+++ src/routes/(api)/api/v1/_schema/+server.ts
@@ -1,7 +1,5 @@
 import { json } from '@sveltejs/kit';
-import { GET as getSchema } from '../admins/schema';
 export async function GET(event) {
-	let schema = getSchema();
 	return json({
 		openapi: '3.1.0',
 		info: {
@@ -10,7 +8,7 @@ export async function GET(event) {
 		},
 		paths: {
 			'/api/v1/admins': {
-				get: schema
+				get: {}
 			}
 			//'/api/v1/admins': api_v1_admins,
 			//'/api/v1/admins/{admin_id}': api_v1_admin
