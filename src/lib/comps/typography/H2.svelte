58f5ecd9b7c020632d0ed530c1976a86e5fd5697
Django Merope <dmerope@gmail.com>
2024-08-07 19:25:09 +0900

    New typography for front page

---
+++ src/lib/comps/typography/H2.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import type { Snippet } from 'svelte';
+	const { class: className = '', children }: { class?: string; children: Snippet } = $props();
+</script>
+
+<h1 class={cn('text-xl md:text-2xl lg:text-3xl xl:text-3xl font-bold', className)}>
+	{@render children()}
+</h1>
