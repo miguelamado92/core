8aa693dac5bcd9079ec6bc62cedb727588423809
Django Merope <dmerope@gmail.com>
2024-08-21 10:15:15 +0900

    Supporting options in addJob

--- src/lib/server/utils/queue/add_job.ts
+++ src/lib/server/utils/queue/add_job.ts
@@ -3,11 +3,17 @@ import { read as readAdmin } from '$lib/server/api/core/admins';
 import { read as readInstance } from '$lib/server/api/core/instances';
 import { Localization, DEFAULT_LANGUAGE } from '$lib/i18n';
 import { dev } from '$app/environment';
+
+type AddQueueOptions = {
+	runAt?: Date;
+};
+
 export default async function (
 	task: string,
 	instance_id: number,
 	data: unknown,
-	admin_id?: number
+	admin_id?: number,
+	options?: AddQueueOptions
 ) {
 	if (!admin_id) {
 		const t = new Localization(DEFAULT_LANGUAGE);
@@ -19,8 +25,12 @@ export default async function (
 		});
 		admin_id = admin.id;
 	}
+	const taskOptions = {
+		...options,
+		maxAttempts: dev ? 1 : 25
+	};
 	if (task[0] === '/') {
 		task = task.slice(1);
 	}
-	await addJob('worker', { task, instance_id, data, admin_id }, { maxAttempts: dev ? 1 : 25 });
+	await addJob('worker', { task, instance_id, data, admin_id }, taskOptions);
 }
