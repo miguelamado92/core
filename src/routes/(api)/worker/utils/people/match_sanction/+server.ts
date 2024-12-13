0f7f940ac2af126aa780c90be6866506525c5676
Django Merope <dmerope@gmail.com>
2024-12-13 15:03:37 +0900

    Updated sanctions checking logic

--- src/routes/(api)/worker/utils/people/match_sanction/+server.ts
+++ src/routes/(api)/worker/utils/people/match_sanction/+server.ts
@@ -1,14 +1,17 @@
-import { json, error } from '$lib/server';
+import { error, pino, json as returnJson } from '$lib/server';
+
 import sendEmailPostmark from '$lib/server/utils/email/send_email_postmark';
 import { v, parse, id } from '$lib/schema/valibot';
 import { read as readAdmin } from '$lib/server/api/core/admins';
 const exportSchema = v.object({
 	adminId: id
 });
+const log = pino('worker:utils/people/match_sanction');
 import {
 	OFAC_API_KEY,
 	OFAC_DATA_SOURCES_ARRAY,
-	OFAC_MATCH_SEND_EMAIL,
+	OFAC_MATCH_SEND_FROM,
+	OFAC_MATCH_SEND_RECIPIENT,
 	OFAC_API_URL
 } from '$env/static/private';
 const DATA_SOURCES = OFAC_DATA_SOURCES_ARRAY.split(',');
@@ -21,7 +24,6 @@ export async function POST(event) {
 			admin_id: parsed.adminId,
 			t: event.locals.t
 		});
-
 		const sendBody = {
 			apiKey: OFAC_API_KEY,
 			minScore: 95, //recommended by ofac-api.com documentation for the best results
@@ -30,7 +32,6 @@ export async function POST(event) {
 			cases: [
 				{
 					id: parsed.adminId,
-					type: 'person',
 					name: admin.full_name,
 					emailAddress: admin.email,
 					nationality: event.locals.instance.country
@@ -39,24 +40,26 @@ export async function POST(event) {
 		};
 		const result = await fetch(OFAC_API_URL, {
 			headers: {
-				ContentType: 'application/json'
+				'Content-Type': 'application/json'
 			},
 			method: 'POST',
 			body: JSON.stringify(sendBody)
 		});
+		log.debug(`OFAC API Response ${result.status}`);
 		const json = await result.json();
 		if (json.error) {
+			log.debug('Error in response');
 			throw new Error(json.error);
 		}
-		if (json.results[0].matches.length > 0) {
+		if (json.results[0].matchCount === 0) {
 			const subject = `OFAC_SANCTION:NOMATCH`;
 			const body = `No match for newly added admin ID ${admin.id} in OFAC database`;
 			await sendEmailPostmark({
-				to: OFAC_MATCH_SEND_EMAIL,
-				from: OFAC_MATCH_SEND_EMAIL,
+				to: OFAC_MATCH_SEND_RECIPIENT,
+				from: OFAC_MATCH_SEND_FROM,
 				subject,
 				html: body,
-				replyTo: OFAC_MATCH_SEND_EMAIL,
+				replyTo: OFAC_MATCH_SEND_FROM,
 				stream: 'outbound'
 			});
 		} else {
@@ -65,16 +68,15 @@ export async function POST(event) {
       
       ${JSON.stringify(json.results[0].matches)}`;
 			await sendEmailPostmark({
-				to: OFAC_MATCH_SEND_EMAIL,
-				from: OFAC_MATCH_SEND_EMAIL,
+				to: OFAC_MATCH_SEND_RECIPIENT,
+				from: OFAC_MATCH_SEND_FROM,
 				subject,
 				html: body,
-				replyTo: OFAC_MATCH_SEND_EMAIL,
+				replyTo: OFAC_MATCH_SEND_FROM,
 				stream: 'outbound'
 			});
 		}
-
-		return json({ success: true });
+		return returnJson({ success: true });
 	} catch (err) {
 		return error(
 			500,
