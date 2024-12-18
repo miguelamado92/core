005cf6b0b51ea454a5041fab5bf53da8006a6d67
Django Merope <dmerope@gmail.com>
2024-12-19 01:01:11 +0900

    Move advanced html metatags and code editing to standalone form and route

--- src/routes/(app)/events/EventsForm.svelte
+++ src/routes/(app)/events/EventsForm.svelte
@@ -64,134 +64,14 @@
 		{@render dateTime()}
 		{@render location()}
 
-		<Collapsible class="mb-4">
-			{#snippet trigger()}
-				{$page.data.t.forms.fields.generic.feature_image.label()}
-			{/snippet}
-			<UploadWidget
-				upload_id={$formData.feature_image_upload_id}
-				onselected={(upload) => {
-					if (upload?.id) $formData.feature_image_upload_id = upload.id;
-					if (upload === null) $formData.feature_image_upload_id = null;
-				}}
-			/>
-		</Collapsible>
-		<Collapsible class="mb-4">
-			{#snippet trigger()}
-				{$page.data.t.forms.fields.metatags.header()}
-			{/snippet}
-			{#if $formData.html_metatags}
-				<Grid cols={1}>
-					<Input
-						{form}
-						name="html_metatags.title"
-						label={$page.data.t.forms.fields.metatags.title.label()}
-						description={$page.data.t.forms.fields.metatags.title.description()}
-						bind:value={$formData.html_metatags.title as string}
-					/>
-					<Textarea
-						{form}
-						name="html_metatags.description"
-						label={$page.data.t.forms.fields.metatags.description.label()}
-						description={$page.data.t.forms.fields.metatags.description.description()}
-						bind:value={$formData.html_metatags.description as string}
-					/>
-					<Input
-						{form}
-						name="html_metatags.subject"
-						label={$page.data.t.forms.fields.metatags.subject.label()}
-						description={$page.data.t.forms.fields.metatags.subject.description()}
-						bind:value={$formData.html_metatags.subject as string}
-					/>
-					<Input
-						{form}
-						name="html_metatags.keywords"
-						label={$page.data.t.forms.fields.metatags.keywords.label()}
-						description={$page.data.t.forms.fields.metatags.keywords.description()}
-						bind:value={$formData.html_metatags.keywords as string}
-					/>
-					<Input
-						{form}
-						name="html_metatags.openGraph.title"
-						label={$page.data.t.forms.fields.metatags.open_graph.title.label()}
-						description={$page.data.t.forms.fields.metatags.open_graph.title.description()}
-						bind:value={$formData.html_metatags.openGraph.title as string}
-					/>
-					<Input
-						{form}
-						name="html_metatags.openGraph.description"
-						label={$page.data.t.forms.fields.metatags.open_graph.description.label()}
-						description={$page.data.t.forms.fields.metatags.open_graph.description.description()}
-						bind:value={$formData.html_metatags.openGraph.description as string}
-					/>
-					<Input
-						{form}
-						name="html_metatags.openGraph.description"
-						label={$page.data.t.forms.fields.metatags.open_graph.description.label()}
-						description={$page.data.t.forms.fields.metatags.open_graph.description.description()}
-						bind:value={$formData.html_metatags.openGraph.description as string}
-					/>
-					<Input
-						{form}
-						name="html_metatags.openGraph.image"
-						label={$page.data.t.forms.fields.metatags.open_graph.image.label()}
-						description={$page.data.t.forms.fields.metatags.open_graph.image.description()}
-						bind:value={$formData.html_metatags.openGraph.image as string}
-					/>
-					<Input
-						{form}
-						name="html_metatags.openGraph.image_alt"
-						label={$page.data.t.forms.fields.metatags.open_graph.image_alt.label()}
-						description={$page.data.t.forms.fields.metatags.open_graph.image_alt.description()}
-						bind:value={$formData.html_metatags.openGraph.image_alt as string}
-					/>
-				</Grid>
-			{/if}
-		</Collapsible>
-
-		<Collapsible class="mb-4">
-			{#snippet trigger()}
-				{$page.data.t.forms.fields.custom_code.header()}
-			{/snippet}
-			{#if $formData.custom_code}
-				<Grid cols={1}>
-					<Code
-						{form}
-						name="custom_code.custom_css"
-						label={$page.data.t.forms.fields.custom_code.custom_css.label()}
-						options={{
-							language: 'css',
-							lineNumbers: true,
-							value: $formData.custom_code.custom_css as string
-						}}
-						bind:value={$formData.custom_code.custom_css as string}
-					/>
-					<Code
-						{form}
-						name="custom_code.custom_js"
-						label={$page.data.t.forms.fields.custom_code.custom_js.label()}
-						options={{
-							language: 'js',
-							lineNumbers: true,
-							value: $formData.custom_code.custom_js as string
-						}}
-						bind:value={$formData.custom_code.custom_js as string}
-					/>
-					<Code
-						{form}
-						name="custom_code.custom_html_head"
-						label={$page.data.t.forms.fields.custom_code.custom_html_head.label()}
-						bind:value={$formData.custom_code.custom_html_head as string}
-					/>
-					<Code
-						{form}
-						name="custom_code.custom_html_body"
-						label={$page.data.t.forms.fields.custom_code.custom_html_body.label()}
-						bind:value={$formData.custom_code.custom_html_body as string}
-					/>
-				</Grid>
-			{/if}
-		</Collapsible>
+		<UploadWidget
+			label={$page.data.t.forms.fields.feature_image.label()}
+			upload_id={$formData.feature_image_upload_id}
+			onselected={(upload) => {
+				if (upload?.id) $formData.feature_image_upload_id = upload.id;
+				if (upload === null) $formData.feature_image_upload_id = null;
+			}}
+		/>
 
 		<Collapsible class="mb-4">
 			{#snippet trigger()}
