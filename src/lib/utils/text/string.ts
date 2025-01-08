aa1cb9f5ad915a288ae5d14430bc6ff54263594f
Django Merope <dmerope@gmail.com>
2025-01-08 21:33:39 +0900

    Simple function to sanitize HTML

--- src/lib/utils/text/string.ts
+++ src/lib/utils/text/string.ts
@@ -4,3 +4,19 @@ export function clampString(str: string, maxLength: number): string {
 	}
 	return str.slice(0, maxLength);
 }
+import { browser } from '$app/environment';
+import DOMPurify from 'dompurify';
+
+/*
+ * @param html - The HTML string to sanitize
+ * @returns The sanitized HTML string
+ */
+export function sanitizeHTML(html: string): string {
+	// We really want this working on the server as well so that we can sanitize the HTML before it goes into the database.
+	// In the future we should use https://github.com/kkomelin/isomorphic-dompurify to make this work on the server and avoid XSS vectors wherever we are likely to rendering user input as HTML.
+	if (browser) {
+		return DOMPurify.sanitize(html, { USE_PROFILES: { html: true } });
+	} else {
+		throw new Error('Sanitizing HTML functionality is only available in the browser');
+	}
+}
