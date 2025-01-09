4a33fec65695d86f2f82e9dee30ed76f08bd8509
Django Merope <dmerope@gmail.com>
2025-01-09 10:56:30 +0900

    Update DOMPurify to use isometric-dompurify
    This allows DOMPurification to happen server-side or during SSG
    rendering

--- src/lib/utils/text/string.ts
+++ src/lib/utils/text/string.ts
@@ -5,20 +5,14 @@ export function clampString(str: string, maxLength: number): string {
 	return str.slice(0, maxLength);
 }
 import { browser } from '$app/environment';
-import DOMPurify from 'dompurify';
+import DOMPurify from 'isomorphic-dompurify';
 
 /*
  * @param html - The HTML string to sanitize
  * @returns The sanitized HTML string
  */
 export function sanitizeHTML(html: string): string {
-	// We really want this working on the server as well so that we can sanitize the HTML before it goes into the database.
-	// In the future we should use https://github.com/kkomelin/isomorphic-dompurify to make this work on the server and avoid XSS vectors wherever we are likely to rendering user input as HTML.
-	if (browser) {
-		return DOMPurify.sanitize(html, { USE_PROFILES: { html: true } });
-	} else {
-		throw new Error('Sanitizing HTML functionality is only available in the browser');
-	}
+	return DOMPurify.sanitize(html, { USE_PROFILES: { html: true } });
 }
 
 export function addLineBreaks(str: string): string {
