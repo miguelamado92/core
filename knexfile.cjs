e7c483e7bdfa275b4dd9b7076a9e62847d7e8173
Django Merope <dmerope@gmail.com>
2025-01-15 12:56:54 +0900

    Added DO cert to staging db configuration
    This is in preparation of a future staging db

--- knexfile.cjs
+++ knexfile.cjs
@@ -22,7 +22,12 @@ module.exports = {
 
 	staging: {
 		client: 'postgresql',
-		connection: process.env.STAGING_DATABASE_URL,
+		connection: {
+			connectionString: process.env.STAGING_DATABASE_URL,
+			ssl: {
+				ca: fs.readFileSync(path.join(__dirname, './ca-certificate.crt')) //needed for SSL in some database providers
+			}
+		},
 		pool: {
 			min: 2,
 			max: 10
