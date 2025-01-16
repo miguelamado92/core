cc7c1178ad45756ef1ca5323fb46bca4d224baa8
dmsynge <dmerope@gmail.com>
2025-01-16 09:49:59 +0900

    Feature/test coverage (#24)
    * Updated vite config to include test coverage
    
    * Vitest coverage plugins and scripts
    
    Because we're not using the latest version of vitest, we need to use
    specific compatible versions of these plugins.
    
    * Added test & coverage output to gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,15 @@ import { sentrySvelteKit } from '@sentry/sveltekit';
 export default defineConfig({
 	plugins: [sentrySvelteKit(), sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		reporters: ['default', 'html'],
+		outputFile: 'vitest/results/index.html',
+		coverage: {
+			include: ['src/**/*.{js,ts}'],
+			exclude: ['src/lib/i18n/localizations/**/*', 'src/zapatos/**/*'],
+			reporter: ['text', 'json', 'html'],
+			reportsDirectory: 'vitest/coverage'
+		}
 	},
 	build: {
 		commonjsOptions: { transformMixedEsModules: true } // Change
