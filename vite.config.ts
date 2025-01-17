b9ff932bc9c65bfe816a48f61c09d22590f2efdf
Django Merope <dmerope@gmail.com>
2025-01-17 21:24:26 +0900

    Inlang plugin to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { sentrySvelteKit } from '@sentry/sveltekit';
+import { paraglide } from '@inlang/paraglide-vite';
 
 export default defineConfig({
-	plugins: [sentrySvelteKit(), sveltekit()],
+	plugins: [
+		sentrySvelteKit(),
+		sveltekit(),
+		paraglide({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide'
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 		reporters: ['default', 'html'],
