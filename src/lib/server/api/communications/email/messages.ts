113d1a855ee04f0fc1942e74d5b69e8173c791ef
dmsynge <dmerope@gmail.com>
2025-01-20 18:45:16 +0900

    Feature/email preview autogen (#31)
    * Created a schema for email preview generation response from OpenAI
    
    * Schema and type for email preview generation job
    
    * Simple alert notification messsage
    
    * More descriptive localization copy for advanced email settings
    
    * Simple openAI function for generating email preview text
    
    * Worker handler for generating email preview text via OpenAI
    
    * Update places where the create or update message functions are called to include newly required params
    
    * Conditionally trigger email preview generation job when creating or updating an email message
    
    This is done conditionally based on determining whether the user has
    provided preview text. The way of figuring this out is slightly
    different for the create compared to the update function, and the
    criteria are explained in comments.
    
    * Cleaning up the email message authoring form and moving everything except the basic things to an advanced settings dropdown
    
    * Cleanup
    
    Moved from name to advanced settings and moved the template selector
    into a snippet to clean up the code a bit
    
    * Added a comment to clarify imperfection in implementation

--- src/lib/server/api/communications/email/messages.ts
+++ src/lib/server/api/communications/email/messages.ts
@@ -1,10 +1,11 @@
 import { db, pool, redis, filterQuery, BelcodaError } from '$lib/server';
 import * as schema from '$lib/schema/communications/email/messages';
 import { parse } from '$lib/schema/valibot';
-
+import { type EmailPreviewOptions } from '$lib/schema/utils/openai';
 function redisString(instanceId: number, messageId: number | 'all') {
 	return `i:${instanceId}:email_messages:${messageId}`;
 }
+import { read as readTemplate } from '$lib/server/api/communications/email/templates';
 
 export async function exists({
 	instanceId,
@@ -36,11 +37,15 @@ export async function exists({
 export async function create({
 	instanceId,
 	defaultTemplateId,
-	body
+	body,
+	queue,
+	t
 }: {
 	instanceId: number;
 	defaultTemplateId: number;
 	body: schema.Create;
+	queue: App.Queue;
+	t: App.Localization;
 }): Promise<schema.Read> {
 	const parsed = parse(schema.create, body);
 	const toInsert = {
@@ -48,10 +53,21 @@ export async function create({
 		template_id: parsed.template_id || defaultTemplateId,
 		...parsed
 	};
+
 	const inserted = await db.insert('communications.email_messages', toInsert).run(pool);
 	const parsedInserted = parse(schema.read, inserted);
 	await redis.del(redisString(instanceId, 'all'));
 	await redis.set(redisString(instanceId, parsedInserted.id), parsedInserted);
+
+	// if the preview text is the same as the template preview text, then we know we are safe to autogenerate the preview
+	const template = await readTemplate({ instanceId, templateId: toInsert.template_id, t: t });
+	if (parsedInserted.preview_text === template.preview_text) {
+		// but, on the contrary, if the preview text is different, that means it was manually set and we should not autogenerate the preview
+		const sendToQueue: EmailPreviewOptions = {
+			emailMessageId: parsedInserted.id
+		};
+		await queue('/utils/openai/generate_email_preview', instanceId, sendToQueue);
+	}
 	return parsedInserted;
 }
 
@@ -59,17 +75,32 @@ export async function update({
 	instanceId,
 	messageId,
 	body,
-	t
+	t,
+	queue
 }: {
 	instanceId: number;
 	messageId: number;
 	body: schema.Update;
 	t: App.Localization;
+	queue: App.Queue;
 }): Promise<schema.Read> {
 	const parsed = parse(schema.update, body);
 	const toUpdate = {
 		...parsed
 	};
+
+	// if the preview text is the same as before, we should autogenerate the preview because it was not manually set
+	/*
+	NOTE: This current implementation is imperfect.
+	If a user provides a manually created preview, saves it, then makes additional changes to the email and saves those changes again --
+	the system will trigger an automatic generation of new preview text which will overwrite their manually created one. 
+	I'm pretty sure avoiding this outcome would require a database migration to add a new flag for the communications.email_messages table. 
+	Given that this is a very low priority feature and custom preview text is very unlikely to be a priority for any of our users in the foreseeable future, 
+	I'm happy leaving it as is and coming back to it later.
+	*/
+	const messageBeforeUpdate = await read({ instanceId, messageId, t });
+	const autogeneratePreview = messageBeforeUpdate.preview_text === parsed.preview_text;
+
 	const updated = await db
 		.update('communications.email_messages', toUpdate, { id: messageId, instance_id: instanceId })
 		.run(pool);
@@ -83,6 +114,12 @@ export async function update({
 	const parsedUpdated = parse(schema.read, updated[0]);
 	await redis.del(redisString(instanceId, 'all'));
 	await redis.set(redisString(instanceId, parsedUpdated.id), parsedUpdated);
+	if (autogeneratePreview) {
+		const sendToQueue: EmailPreviewOptions = {
+			emailMessageId: parsedUpdated.id
+		};
+		await queue('/utils/openai/generate_email_preview', instanceId, sendToQueue);
+	}
 	return parsedUpdated;
 }
 
