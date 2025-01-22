57131fe1db644dc6c354b4d37699eca61c452755
Kenneth Lebu <kenlebu@gmail.com>
2025-01-22 19:54:49 +0300

    Add util to get whatsapp link

---
+++ src/lib/utils/text/whatsapp.ts
@@ -0,0 +1,18 @@
+import type { Read as Instance } from '$lib/schema/core/instance';
+import type { Read as Event } from '$lib/schema/events/events';
+import { formatDate } from './date';
+
+export function renderRegistrationLink(
+	instance: Instance,
+	event: Event
+): { text: string; url: string } {
+	const whatsappPhoneNumberId = instance.settings.communications.whatsapp.phone_number_id?.replace(
+		'+',
+		''
+	);
+	const text = `Hi! I'm interested in ${event.name} [#4fg6XFDE32:${event.id}] on ${formatDate(event.starts_at)}`;
+	return {
+		text: 'Whatsapp registration link',
+		url: `https://wa.me/${whatsappPhoneNumberId}?text=${encodeURIComponent(text)}`
+	};
+}
