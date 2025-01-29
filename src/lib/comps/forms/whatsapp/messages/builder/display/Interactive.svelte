f88071ce30c47d5bdd477c11f209bd79c58661a0
Django Merope <dmerope@gmail.com>
2025-01-29 23:14:10 +0900

    Component for displaying message buttons
    This is only for displaying the buttons, not editing them or anything
    like that. Therefore, it just needs to figure out if the message object
    has buttons and to display them

---
+++ src/lib/comps/forms/whatsapp/messages/builder/display/Interactive.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Reply from 'lucide-svelte/icons/reply';
+	import { type Read, type List } from '$lib/schema/communications/whatsapp/messages';
+	const { messages, index }: { messages: Read[]; index: number } = $props();
+</script>
+
+{#if messages[index].message.type === 'interactive'}
+	{#if messages[index].message.interactive.type === 'button'}
+		{#each messages[index].message.interactive.action.buttons as button, i}
+			<div class=" flex items-center gap-1 border-t">
+				<Reply size={16} />
+				<div>{button.title}</div>
+			</div>
+		{/each}
+	{/if}
+{/if}
