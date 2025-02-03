ed330fea9860d9ddea2511e2efb90cbb90711fce
Django Merope <dmerope@gmail.com>
2025-02-03 21:17:46 +0900

    New version of component to create message

---
+++ src/lib/comps/forms/whatsapp/messages/builder/CreateNewMessage.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import * as m from '$lib/paraglide/messages';
+	import { type Message } from '$lib/schema/communications/whatsapp/elements/message';
+	type Props = {
+		oncreate: (message: Message) => void;
+	};
+	let { oncreate }: Props = $props();
+	import Button from '$lib/comps/ui/button/button.svelte';
+	import Input from '$lib/comps/ui/input/input.svelte';
+
+	let message: Message = $state({
+		type: 'text',
+		text: {
+			body: '',
+			preview_url: true
+		}
+	});
+</script>
+
+<div class="flex gap-4 items-baseline">
+	<div class="flex-grow">
+		{#if message.type === 'text'}
+			<Input type="text" bind:value={message.text.body} class="w-full" />
+		{/if}
+	</div>
+	<div>
+		<Button
+			class="btn btn-primary"
+			onclick={() => {
+				oncreate(message);
+			}}
+		>
+			{m.tidy_watery_ape_lift()}
+		</Button>
+	</div>
+</div>
