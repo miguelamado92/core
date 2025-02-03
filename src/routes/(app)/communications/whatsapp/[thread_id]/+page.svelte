122640d681f2aa7ff5580c1e62b612c2bdb24d55
Django Merope <dmerope@gmail.com>
2025-02-03 21:19:29 +0900

    Making the +page.svelte component work for new system
    - Update imports to be new components
    - Move most of the logic out to external /actions/*.ts files
    - Add loading indicators and better error handling
    - Add more consistent toast messages on success and error

--- src/routes/(app)/communications/whatsapp/[thread_id]/+page.svelte
+++ src/routes/(app)/communications/whatsapp/[thread_id]/+page.svelte
@@ -1,94 +1,76 @@
 <script lang="ts">
 	const { data } = $props();
-	let actions = $state({ ...data.templateMessage.actions });
-	let selectedMessageId: string | null = $state(null);
 
-	import PageHeader from '$lib/comps/layout/PageHeader.svelte';
-	import Template from '$lib/comps/forms/whatsapp/templates/Template.svelte';
+	import { getFlash } from 'sveltekit-flash-message';
+	import { page } from '$app/state';
+	const flash = getFlash(page);
+
 	import { type Template as TemplateType } from '$lib/schema/communications/whatsapp/elements/template_message';
-	import { createMessageComponentsFromTemplateComponents } from '$lib/comps/forms/whatsapp/templates/components';
-	import TemplateSelectDropdown from '$lib/comps/forms/whatsapp/templates/select/Dropdown.svelte';
-	import Message from '$lib/comps/forms/whatsapp/messages/builder/Message.svelte';
-
-	let selectedIndex: number = $state(-1);
-	let templateId = $state(data.thread.template_id);
-	const template = $derived(data.templates.items.find((template) => template.id === templateId));
-
-	import { type Read as ReadThread } from '$lib/schema/communications/whatsapp/threads';
-
-	function updateComponents(
-		template: (typeof data.templates.items)[0],
-		actions: ReadThread['actions'] = data.templateMessage.actions
-	) {
-		return createMessageComponentsFromTemplateComponents(
-			template.message.components,
-			actions,
-			data.templateMessage.message
-		);
-	}
-
-	//not sure about this warning... looks like it was fixed https://github.com/sveltejs/svelte/pull/11540
-	const startingCompoments = () => {
-		return template ? updateComponents(template, actions).components : [];
-	};
-	import { parse } from '$lib/schema/valibot';
-	import { list as messageListSchema } from '$lib/schema/communications/whatsapp/messages';
-
-	async function fetchMessages() {
-		const res = await fetch(`/api/v1/communications/whatsapp/threads/${data.thread.id}/messages`);
-		const body = await res.json();
-		const parsed = parse(messageListSchema, body);
-		messages = parsed.items;
-	}
-	import { onMount } from 'svelte';
-	onMount(fetchMessages);
-
-	let components: TemplateType['components'] = $state(startingCompoments());
-	import MessagesList from '$lib/comps/forms/whatsapp/messages/builder/MessageList.svelte';
-	let loading: boolean = $state(false);
-
 	import { type List } from '$lib/schema/communications/whatsapp/messages';
 
-	let messages: List['items'] = $state([]);
+	import * as componentActions from '$lib/comps/forms/whatsapp/messages/builder/actions/components';
+	import * as threadActions from '$lib/comps/forms/whatsapp/messages/builder/actions/thread';
+	import { createNewMessage } from '$lib/comps/forms/whatsapp/messages/builder/actions/messages';
+
+	import PageHeader from '$lib/comps/layout/PageHeader.svelte';
+	import Loading from '$lib/comps/helpers/Loading.svelte';
 	import Button from '$lib/comps/ui/button/button.svelte';
 
-	import NewMessageForm from '$lib/comps/forms/whatsapp/messages/NewMessageForm.svelte';
-	import {
-		createNewMessage,
-		fetchMessages as updateMessageThread
-	} from '$lib/comps/forms/whatsapp/messages/builder/actions/messages';
-	async function updateThread() {
+	import TemplateSelectDropdown from '$lib/comps/forms/whatsapp/messages/builder/templates/select/Dropdown.svelte';
+	import Message from '$lib/comps/forms/whatsapp/messages/builder/Message.svelte';
+	import CreateNewMessage from '$lib/comps/forms/whatsapp/messages/builder/CreateNewMessage.svelte';
+	import Template from '$lib/comps/forms/whatsapp/messages/builder/templates/Template.svelte';
+
+	const startingCompoments = () => {
+		return template
+			? componentActions.createMessageComponentsFromTemplateComponents(
+					template.message.components,
+					actions, //default
+					data.templateMessage.message
+				).components
+			: [];
+	};
+
+	import { onMount } from 'svelte';
+	onMount(async () => {
 		loading = true;
-		const res = await fetch(`/api/v1/communications/whatsapp/threads/${data.thread.id}`, {
-			method: 'PUT',
-			body: JSON.stringify({
-				template_id: templateId
-			})
-		});
-		if (!res.ok) {
-			console.error('Failed to update thread', res);
-		}
-		if (data.templateMessage.message.type === 'template') {
-			const messageRes = await fetch(
-				`/api/v1/communications/whatsapp/messages/${data.templateMessage.id}`,
-				{
-					method: 'PUT',
-					body: JSON.stringify({
-						actions: actions,
-						message: {
-							template_id: templateId,
-							...data.templateMessage.message,
-							template: {
-								...data.templateMessage.message.template,
-								name: template?.message.name,
-								components: components
-							}
-						}
-					})
-				}
-			);
-		}
+		const fetchedMessages = await threadActions.fetchMessages(data.thread.id); //to avoid messages become a Promise type
+		messages = fetchedMessages;
 		loading = false;
+	});
+
+	let actions = $state({ ...data.templateMessage.actions });
+	let templateMessage = $state({ ...data.templateMessage.message });
+	let selectedMessageId: string | null = $state(null);
+	let templateId = $state(data.thread.template_id);
+	const template = $derived(data.templates.items.find((template) => template.id === templateId));
+	let components: TemplateType['components'] = $state(startingCompoments());
+	let loading: boolean = $state(false);
+	let messages: List['items'] = $state([]);
+
+	async function saveThread() {
+		try {
+			loading = true;
+			if (template) {
+				await threadActions.updateThread({
+					templateMessage,
+					actions,
+					templateName: template.name,
+					components,
+					messageId: data.thread.template_message_id
+				});
+				$flash = { type: 'success', message: 'Message saved' };
+			}
+		} catch (err) {
+			console.log(err);
+			if (err instanceof Error) {
+				$flash = { type: 'error', message: err.message };
+			} else {
+				$flash = { type: 'error', message: 'An error occurred' };
+			}
+		} finally {
+			loading = false;
+		}
 	}
 </script>
 
@@ -98,46 +80,66 @@
 			<Button variant="secondary" href="/communications/whatsapp/{data.thread.id}/sends">
 				Send
 			</Button>
-			<Button onclick={updateThread}>{data.t.forms.buttons.save()}</Button>
+			<Button onclick={saveThread}>{data.t.forms.buttons.save()}</Button>
 		</div>
 	{/snippet}
 </PageHeader>
 <div class="relative">
-	<div class="w-full mt-6">
+	<div class="w-full mt-6 mb-2">
 		<div class="flex justify-center mb-4">
 			<TemplateSelectDropdown
 				value={templateId}
 				onselect={async (template) => {
-					loading = true;
 					templateId = template.id;
-					const output = updateComponents(template, actions);
+					const output = componentActions.createMessageComponentsFromTemplateComponents(
+						template.message.components,
+						actions,
+						templateMessage
+					);
 					components = output.components;
 					actions = output.actions;
-					await updateThread();
+					await saveThread();
 					loading = false;
 				}}
 			/>
 		</div>
-		<Template bind:selectedIndex bind:actions {messages} {template} bind:components />
+		<div
+			class="flex cursor-pointer"
+			onclick={() => (selectedMessageId = 'template')}
+			role="button"
+			tabindex={0}
+			onkeydown={() => (selectedMessageId = 'template')}
+		>
+			<Template
+				{messages}
+				bind:actions
+				bind:selectedMessageId
+				{template}
+				bind:components
+				onSaveTemplate={saveThread}
+			/>
+		</div>
 	</div>
 
 	<div class="grid grid-cols-1 gap-2">
 		{#each messages as message, i}
 			<div
+				class="flex cursor-pointer"
 				onclick={() => (selectedMessageId = message.id)}
 				role="button"
 				tabindex={i + 1}
 				onkeydown={() => (selectedMessageId = message.id)}
 			>
-				<Message bind:selectedMessageId index={i} bind:messages />
+				<Message bind:loading bind:selectedMessageId index={i} bind:messages />
 			</div>
 		{/each}
 		<div class="mt-4">
-			<NewMessageForm
-				threadId={data.thread.id}
+			<CreateNewMessage
 				oncreate={async (message) => {
+					loading = true;
 					await createNewMessage(message, data.thread.id);
-					messages = await updateMessageThread(data.thread.id);
+					messages = await threadActions.fetchMessages(data.thread.id);
+					loading = false;
 				}}
 			/>
 		</div>
@@ -145,6 +147,6 @@
 	{#if loading}<div
 			class="absolute inset-0 z-10 opacity-50 bg-white flex justify-center items-center"
 		>
-			Loading...
+			<Loading />
 		</div>{/if}
 </div>
