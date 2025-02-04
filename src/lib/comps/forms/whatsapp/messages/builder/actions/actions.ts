90b8df0a0eb0d4a7b1cc85287ba9652a21e7eab9
Django Merope <dmerope@gmail.com>
2025-02-04 15:22:21 +0900

    Refactor to avoid mutating function param state

--- src/lib/comps/forms/whatsapp/messages/builder/actions/actions.ts
+++ src/lib/comps/forms/whatsapp/messages/builder/actions/actions.ts
@@ -1,55 +1,42 @@
 import { type Read } from '$lib/schema/communications/whatsapp/messages';
 
 export function createWhatsAppMessageAction(id: string, actions: Read['actions']): Read['actions'] {
+	let newActions = { ...actions };
 	if (Array.isArray(actions[id])) {
 		// This is the new action that we are adding to the actions array
-		let newActions = actions;
-		newActions[id] = newActions[id].concat([
-			{
-				type: 'send_whatsapp_message',
-				message_id: ''
-			}
-		]);
-		return newActions;
+		newActions[id] = [...actions[id], { type: 'send_whatsapp_message', message_id: '' }];
 	} else {
 		// If there are no actions in the actions array, we create a new array with the new action
-		let newActions = actions;
 		newActions[id] = [
 			{
 				type: 'send_whatsapp_message',
 				message_id: ''
 			}
 		];
-		return newActions;
 	}
+	return newActions;
 }
 
 export function createEventRegistrationAction(
 	id: string,
 	actions: Read['actions']
 ): Read['actions'] {
+	let newActions = { ...actions };
 	if (Array.isArray(actions[id])) {
 		// This is the new action that we are adding to the actions array
-		let newActions = actions;
-		newActions[id] = newActions[id].concat([
-			{
-				type: 'register_for_event',
-				event_id: 0
-			}
-		]);
-		return newActions;
+		newActions[id] = [...actions[id], { type: 'register_for_event', event_id: 0 }];
 	} else {
 		// If there are no actions in the actions array, we create a new array with the new action
-		let newActions = actions;
 		newActions[id] = [
 			{
 				type: 'register_for_event',
 				event_id: 0
 			}
 		];
-		return newActions;
 	}
+	return newActions;
 }
+
 export function updateEventRegistrationAction(
 	action: Read['actions'][string][number],
 	eventId: number
