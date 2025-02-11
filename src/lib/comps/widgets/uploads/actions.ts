7a2b58ed58010989694a2db531c335a4e5c1b89f
dmsynge <dmerope@gmail.com>
2025-02-11 09:54:34 +0900

    Patch/upload widget (#44)
    * Remove unnecessary logging of SQL on event update
    
    * Simplify parsing to remove incorrect validation error that was causing bug
    
    Changes in the underlying file upload component created a bug in which
    parsing for the uploads would fail silently, and the file wouldn't be
    added as a website.upload row in the database, but would still be
    uploaded to S3 and previewed.
    
    This fixes it by just parsing the input from the file upload component
    directly. Because the new component is typesafe, the parseUnknownData
    helper function that was causing the problem is no longer needed.

--- src/lib/comps/widgets/uploads/actions.ts
+++ src/lib/comps/widgets/uploads/actions.ts
@@ -22,24 +22,6 @@ export async function loadSingle(id: number): Promise<Read> {
 	return parsed;
 }
 
-function parseUnknownData(data: unknown, t: App.Localization): Create {
-	try {
-		const parsed = parse(create, {
-			// @ts-ignore - we're parsing anyway
-			file_name: data.file_name || '',
-			// @ts-ignore - we're parsing anyway
-			size: data.size,
-			// @ts-ignore - we're parsing anyway
-			url: data.url,
-			// @ts-ignore - we're parsing anyway
-			mime_type: data.mimeType
-		});
-		return parsed;
-	} catch (err) {
-		throw new Error(t.errors.creating_data());
-	}
-}
-
 export async function upload(
 	data: { fileName: string; size: number; type: string; url: string },
 	t: App.Localization
@@ -50,13 +32,13 @@ export async function upload(
 		url: data.url,
 		mime_type: data.type
 	};
-	const parsed = parseUnknownData(data, t);
+	const parsed = parse(create, createFile);
 	const response = await fetch('/api/v1/website/uploads', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
 		},
-		body: JSON.stringify(createFile)
+		body: JSON.stringify(parsed)
 	});
 	if (!response.ok) {
 		throw new Error(`Failed to upload: ${response.statusText}`);
