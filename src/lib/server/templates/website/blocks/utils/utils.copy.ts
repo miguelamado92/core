ee1d46578433faab23d156cd98e8d48117657462
Django Merope <dmerope@gmail.com>
2025-03-17 14:28:55 +0900

    Re-exported the all_rights_reserved translated page copy
    For some reason, it was deleted in another commit. This is re-adding it.

--- src/lib/server/templates/website/blocks/utils/utils.copy.ts
+++ src/lib/server/templates/website/blocks/utils/utils.copy.ts
@@ -2,7 +2,7 @@ import * as m from '$lib/paraglide/messages';
 export default function () {
 	return {
 		footer: {
-			all_rights_reserved: m.curly_such_horse_cherish()
+			all_rights_reserved: m.broad_acidic_beetle_skip()
 		}
 	};
 }
