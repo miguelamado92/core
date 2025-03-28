1f10bfb94fcbbae9909e4d0894872e6afa865baa
Django Merope <dmerope@gmail.com>
2025-03-28 23:47:11 +0900

    Removed unused element from triggerAction schema

--- src/lib/schema/communications/actions/actions.ts
+++ src/lib/schema/communications/actions/actions.ts
@@ -54,7 +54,6 @@ export const actionTriggerType = v.picklist(['whatsapp_message']);
 
 export const triggerAction = v.object({
 	type: actionTriggerType,
-	received_whatsapp_message_id: uuid,
 	person_id: id,
 	action_id: uuid,
 	data: v.optional(v.record(v.string(), v.any())) // Specify object with any string keys and values
