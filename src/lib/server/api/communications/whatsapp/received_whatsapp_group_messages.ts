e4c557aa7fb890181942e12858368b2283bb8406
Kenneth Lebu <kenlebu@gmail.com>
2025-04-09 12:49:20 +0300

    remove comment

--- src/lib/server/api/communications/whatsapp/received_whatsapp_group_messages.ts
+++ src/lib/server/api/communications/whatsapp/received_whatsapp_group_messages.ts
@@ -55,7 +55,6 @@ export async function list({
 			return parse(schema.list, cached);
 		}
 	}
-	// ?Qn: Do we need to know if a deleted person received a message?
 	const list = await db
 		.select(
 			'communications.received_whatsapp_group_messages',
