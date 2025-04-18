7d9851d690904e99bd5aaf0d0683272f0886336f
Django Merope <dmerope@gmail.com>
2025-04-18 14:36:17 +0900

    Update messages schemas and types for named templates rather than templates from the database

--- src/lib/schema/communications/email/messages.ts
+++ src/lib/schema/communications/email/messages.ts
@@ -1,16 +1,38 @@
+import { sendDetail } from '$lib/schema/communications/email/sent_emails';
 import {
 	v,
+	count,
 	id,
 	timestamp,
 	email,
 	longString,
 	shortString,
-	mediumString
+	mediumString,
+	shortStringNotEmpty,
+	emailAttachments,
+	jsonSchemaObject
 } from '$lib/schema/valibot';
+
+export const emailTemplates = [
+	'main',
+	'main-nologo',
+	'minimal',
+	'transactional',
+	'event-registration-reminder',
+	'event-cancelled'
+] as const;
+export const emailTemplateName = v.picklist(emailTemplates);
+export type EmailTemplateName = v.InferOutput<typeof emailTemplateName>;
+export const emailTemplateNameEvent = v.picklist([
+	'event-reminder-registration',
+	'event-cancelled'
+]);
+export type EmailTemplateNameEvent = v.InferOutput<typeof emailTemplateNameEvent>;
+
 export const base = v.object({
 	id: id,
 	instance_id: id,
-	template_id: id,
+	template_name: emailTemplateName,
 	name: shortString,
 	from: mediumString,
 	reply_to: mediumString,
@@ -27,22 +49,26 @@ export const base = v.object({
 export const read = v.omit(base, ['instance_id']);
 export type Read = v.InferOutput<typeof read>;
 
-export const list = v.array(v.omit(base, ['instance_id', 'html', 'text']));
+export const list = v.object({
+	count: count,
+	items: v.array(v.omit(base, ['instance_id', 'html', 'text']))
+});
 export type List = v.InferOutput<typeof list>;
 
 export const create = v.object({
-	template_id: v.optional(base.entries.template_id),
 	name: base.entries.name,
 	from: base.entries.from,
+	template_name: v.optional(base.entries.template_name, 'main'),
 	reply_to: base.entries.reply_to,
 	subject: base.entries.subject,
-	preview_text: base.entries.preview_text,
+	preview_text: v.optional(base.entries.preview_text, ''),
 	html: base.entries.html,
 	text: base.entries.text,
 	point_person_id: base.entries.point_person_id,
 	use_html_for_plaintext: v.optional(base.entries.use_html_for_plaintext, true)
 });
 export type Create = v.InferOutput<typeof create>;
+export type CreateInput = v.InferInput<typeof create>;
 
 export const update = v.partial(create);
 export type Update = v.InferOutput<typeof update>;
@@ -51,8 +77,19 @@ export const sendTestEmail = v.object({
 	email: email,
 	message: read,
 	context: v.object({
-		event_id: v.optional(v.nullable(id)),
 		person_id: v.optional(v.nullable(id))
 	})
 });
 export type SendTestEmail = v.InferOutput<typeof sendTestEmail>;
+
+export const emailTemplateMessage = v.object({
+	person_id: id,
+	send_id: v.optional(id),
+	template: emailTemplateName,
+	context: jsonSchemaObject,
+	from: v.optional(shortStringNotEmpty),
+	send_details: sendDetail,
+	reply_to: v.optional(v.nullable(email), null),
+	attachments: v.optional(v.array(emailAttachments))
+});
+export type EmailTemplateMessage = v.InferOutput<typeof emailTemplateMessage>;
