b9649a79812c73e5362eccb821ea54bec8e2e72f
Django Merope <dmerope@gmail.com>
2025-04-18 14:58:18 +0900

    Remove email message editing components from petition routes

--- src/routes/(app)/petitions/[petition_id]/edit/notifications/+page.svelte
+++ src/routes/(app)/petitions/[petition_id]/edit/notifications/+page.svelte
@@ -24,23 +24,6 @@
 	{/snippet}
 </PageHeader>
 
-{#snippet editPreviewButtons(messageId: number, visible: boolean)}
-	{#if visible}
-		<div class="flex items-center gap-2">
-			<Button href="/communications/email/messages/{messageId}" target="_blank">
-				{m.giant_misty_shrimp_stop()}
-			</Button>
-			<Button
-				variant="outline"
-				href="/preview/email/{messageId}?petition_id={$page.params.petition_id}"
-				target="_blank"
-			>
-				{m.alive_silly_antelope_build()}
-			</Button>
-		</div>
-	{/if}
-{/snippet}
-
 <form method="post" use:enhance>
 	<Grid cols={1} class="mt-4">
 		<Error error={$message} />
@@ -53,11 +36,6 @@
 				description={m.moving_bold_turtle_bend()}
 				bind:checked={$formData.send_autoresponse_email as boolean}
 			/>
-
-			{@render editPreviewButtons(
-				data.petition.autoresponse_email.id,
-				$formData.send_autoresponse_email as boolean
-			)}
 		</Grid>
 
 		<div class="flex justify-end">
