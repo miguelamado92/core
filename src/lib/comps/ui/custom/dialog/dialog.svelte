6cb689922c92694713216f0173b023bad159385b
Django Merope <dmerope@gmail.com>
2025-04-18 11:45:19 +0900

    Created confirmation dialog component

---
+++ src/lib/comps/ui/custom/dialog/dialog.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { Button } from '$lib/comps/ui/button';
+	import * as Dialog from '$lib/comps/ui/dialog';
+	import { type Snippet } from 'svelte';
+	let {
+		open = $bindable(false),
+		children,
+		title,
+		description
+	}: { open: boolean; children: Snippet; title: string; description?: string } = $props();
+
+	function closeDialog() {
+		open = false;
+	}
+
+	import X from 'lucide-svelte/icons/x';
+</script>
+
+<Dialog.Root bind:open>
+	<Dialog.Content>
+		<Dialog.Header>
+			<Dialog.Title>{title}</Dialog.Title>
+			{#if description}
+				<Dialog.Description>
+					{@html description}
+				</Dialog.Description>
+			{/if}
+		</Dialog.Header>
+		{@render children()}
+	</Dialog.Content>
+</Dialog.Root>
