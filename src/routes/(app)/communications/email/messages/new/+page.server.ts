19c41da29585ec17c30500ce17be4f9cb794a33f
Django Merope <dmerope@gmail.com>
2025-04-18 15:05:56 +0900

    Create new routes for creating new email messages

---
+++ src/routes/(app)/communications/email/messages/new/+page.server.ts
@@ -0,0 +1,30 @@
+import { formAction, valibot, superValidate, redirect } from '$lib/server';
+import { create, read } from '$lib/schema/communications/email/messages';
+import { parse } from '$lib/schema/valibot';
+import * as m from '$lib/paraglide/messages';
+//load form
+export async function load(event) {
+	const form = await superValidate({ point_person_id: event.locals.admin.id }, valibot(create));
+	return { form };
+}
+
+//handle form submission to create new message...
+export const actions = {
+	default: async function (event) {
+		const result = await formAction({
+			method: 'POST',
+			url: '/api/v1/communications/email/messages',
+			event,
+			inputSchema: create
+		});
+		if (result.error) {
+			return result.output;
+		}
+
+		const parsed = parse(read, result.output);
+		return redirect(event, {
+			location: `/communications/email/messages/${parsed.id}`,
+			message: m.flat_sleek_millipede_agree()
+		});
+	}
+};
---
+++ src/routes/(app)/communications/email/messages/new/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let data;
+	import PageHeader from '$lib/comps/layout/PageHeader.svelte';
+
+	import EditMessageForm from '$lib/comps/forms/email/EmailEdit.svelte';
+	import * as m from '$lib/paraglide/messages';
+</script>
+
+<PageHeader title={m.ideal_fuzzy_grebe_cut()} />
+<EditMessageForm
+	mode="create"
+	formObject={data.form}
+	actionUrl={`/communications/email/messages/new`}
+/>
