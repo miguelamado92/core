7a34408255e21c4b2163b843462e44003191945b
Django Merope <dmerope@gmail.com>
2025-04-18 15:02:24 +0900

    Redirect /communications/email to /communications/email/messages
    Previously we'd had send routes at /communications/email but this makes
    it much more logical, and a simple redirect makes sense

--- src/routes/(app)/communications/email/+page.server.ts
+++ src/routes/(app)/communications/email/+page.server.ts
@@ -1,10 +1,4 @@
-import { filter, loadError } from '$lib/server';
-import { list } from '$lib/schema/communications/email/sends';
-import { parse } from '$lib/schema/valibot';
+import { redirect } from '@sveltejs/kit';
 export async function load(event) {
-	const result = await event.fetch(filter(`/api/v1/communications/email/sends`, event.url));
-	if (!result.ok) return loadError(result);
-	const body = await result.json();
-	const parsed = parse(list, body);
-	return { sends: parsed };
+	return redirect(302, '/communications/email/messages');
 }
