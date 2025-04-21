cf308c189718f7f770dd97e4000f8f74ab9a16b5
Django Merope <dmerope@gmail.com>
2025-04-21 13:14:09 +0900

    Add mocks to email template type that was causing error in tests

--- src/routes/(api)/api/v1/settings/secrets/secrets.test.ts
+++ src/routes/(api)/api/v1/settings/secrets/secrets.test.ts
@@ -10,6 +10,10 @@ vi.mock('$lib/server/api/core/instances', () => ({
 	update: vi.fn()
 }));
 
+vi.mock('$lib/schema/communications/email/messages', () => ({
+	emailTemplates: ['main'] // mocks imported email template schema
+}));
+
 vi.mock('$lib/server', () => ({
 	json: vi.fn((data) => ({
 		status: 200,
